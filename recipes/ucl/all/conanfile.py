8ce944f12f58c84e4d12cbd115fe52ac655773b9
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-09-07 23:24:47 +0200

    ucl: msvc fixes

--- recipes/ucl/all/conanfile.py
+++ recipes/ucl/all/conanfile.py
@@ -1,4 +1,6 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans.errors import ConanInvalidConfiguration
+from contextlib import contextmanager
 import os
 
 
@@ -18,6 +20,7 @@ class UclConan(ConanFile):
         "shared": False,
         "fPIC": True,
     }
+    exports_sources = "patches/**"
 
     _autotools = None
 
@@ -29,44 +32,73 @@ class UclConan(ConanFile):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
-
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+        if self.settings.os == "Windows" and self.options.shared:
+            raise ConanInvalidConfiguration("ucl cannot be built as a shared library on Windows")
         del self.settings.compiler.cppstd
         del self.settings.compiler.libcxx
 
     def build_requirements(self):
         if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
             self.build_requires("msys2/20200517")
+        if self.settings.compiler == "Visual Studio":
+            self.build_requires("automake/1.16.2")
+        self.build_requires("autoconf/2.69")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
 
+    @contextmanager
+    def _build_context(self):
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self.settings):
+                env = {
+                    "CC": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
+                    "CXX": "{} cl -nologo".format(tools.unix_path(self.deps_user_info["automake"].compile)),
+                    "LD": "link -nologo",
+                    "AR": "lib",
+                }
+                with tools.environment_append(env):
+                    yield
+        else:
+            yield
+
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
         self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        self._autotools.libs = []
         configure_args = [
-            "--with-pic" if self.options.get_safe("fPIC") in (True, None, ) else "--without-pic",
             "--enable-shared" if self.options.shared else "--disable-shared",
             "--disable-static" if self.options.shared else "--enable-static",
         ]
-        self._autotools.flags.append("-std=c90")
+        if self.settings.compiler != "Visual Studio":
+            self._autotools.flags.append("-std=c90")
         self._autotools.configure(configure_dir=os.path.join(self.source_folder, self._source_subfolder), args=configure_args)
         return self._autotools
 
     def build(self):
-        autotools = self._configure_autotools()
-        autotools.make()
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        with tools.chdir(self._source_subfolder):
+            self.run("autoconf", run_environment=True, win_bash=tools.os_info.is_windows)
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.make()
 
     def package(self):
         self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        autotools = self._configure_autotools()
-        autotools.install()
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.install()
 
         os.unlink(os.path.join(self.package_folder, "lib", "libucl.la"))
 
     def package_info(self):
-        self.cpp_info.libs = ["ucl"]
+        libname = "ucl"
+        if self.settings.compiler == "Visual Studio":
+            libname = "lib" + libname
+        self.cpp_info.libs = [libname]
