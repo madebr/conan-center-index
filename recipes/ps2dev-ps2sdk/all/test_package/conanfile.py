994a6a3f6cad35ef40145acfc9b494ffd8e44096
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-12-03 04:03:22 +0100

    ps2dev-ps2sdk: add ps2dev-ps2sdk recipe

---
+++ recipes/ps2dev-ps2sdk/all/test_package/conanfile.py
@@ -0,0 +1,22 @@
+from conans import CMake, ConanFile, tools
+import os
+
+
+class TestPackage(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake"
+
+    def build_requirements(self):
+        self.build_requires("ps2dev-ps2toolchain/cci.20201030")
+
+    def build(self):
+        with tools.environment_append({"PS2SDK": self.deps_user_info["ps2dev-ps2sdk"].PS2SDK}):
+            cmake = CMake(self)
+            cmake.verbose = True
+
+            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = os.path.join(self.source_folder, "ee-toolchain.cmake").replace("\\", "/")
+            cmake.configure()
+            cmake.build()
+
+    def test(self):
+        pass
