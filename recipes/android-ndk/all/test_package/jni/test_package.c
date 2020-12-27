fabcd010c7fef89f5ae1c72f62d3226910545d64
Anonymous Maarten <madebr@users.noreply.github.com>
2020-12-27 15:55:56 +0100

    android-ndk: fix typo in test_package

--- recipes/android-ndk/all/test_package/jni/test_package.c
+++ recipes/android-ndk/all/test_package/jni/test_package.c
@@ -2,5 +2,5 @@
 
 __attribute__((visibility("default")))
 void test_package_log_message(const char *msg) {
-    __android_log_print(ANDROID_LOG_DEBUG, "test_pacakge", "%s", msg);
+    __android_log_print(ANDROID_LOG_DEBUG, "test_package", "%s", msg);
 }
