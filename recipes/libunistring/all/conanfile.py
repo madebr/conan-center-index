480064fa8506fe794ff0321ebe3c9bacc0ad9bc0
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-01-02 01:18:40 +0100

    libunistring WIPPP

--- recipes/libunistring/all/conanfile.py
+++ recipes/libunistring/all/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
 from conans.errors import ConanInvalidConfiguration
+import contextlib
 import os
 
 
@@ -8,7 +9,7 @@ class LibUnistringConan(ConanFile):
     description = "This library provides functions for manipulating Unicode strings and for manipulating C strings according to the Unicode standard."
     homepage = "https://www.gnu.org/software/libunistring/"
     topics = ("conan", "libunistring", "unicode", "string")
-    license = "LGPL-3.0-or-later"
+    license = ("LGPL-3.0-or-later", "GPL-3.0-or-later")
     url = "https://github.com/conan-io/conan-center-index"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -28,8 +29,8 @@ class LibUnistringConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        if self.settings.compiler == "Visual Studio":
-            raise ConanInvalidConfiguration("Visual Studio is unsupported")
+        # if self.settings.compiler == "Visual Studio":
+        #     raise ConanInvalidConfiguration("Visual Studio is unsupported")
         if self.settings.os == "Windows" and self.options.shared:
             raise ConanInvalidConfiguration("Shared build on Windows is not supported")
         if self.options.shared:
@@ -37,35 +38,59 @@ class LibUnistringConan(ConanFile):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
 
+    def build_requirements(self):
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/20200517")
+        if self.settings.compiler == "Visual Studio":
+            self.build_requires("automake/1.16.3")
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("libunistring-{}".format(self.version), self._source_subfolder)
 
-    def build_requirements(self):
-        if tools.os_info.is_windows and not "CONAN_BASH_PATH" in os.environ:
-            self.build_requires("msys2/20190524")
+    @contextlib.contextmanager
+    def _build_context(self):
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self):
+                env = {
+                    "CC": "{} cl -nologo".format(self.deps_user_info["automake"].compile.replace("\\", "/")),
+                    "CXX": "{} cl -nologo".format(self.deps_user_info["automake"].compile.replace("\\", "/")),
+                    "AR": "{} lib".format(self.deps_user_info["automake"].ar_lib.replace("\\", "/")),
+                    "LD": "link",
+                    "NM": "dumpbin -symbols",
+                    "STRIP": ":",
+                    "RANLIB": ":",
+                }
+                with tools.environment_append(env):
+                    yield
+        else:
+            yield
 
     def _configure_autotools(self):
         if self._autotools:
             return self._autotools
         self._autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-        conf_args = []
-        if self.options.shared:
-            conf_args.extend(["--enable-shared", "--disable-static"])
-        else:
-            conf_args.extend(["--disable-shared", "--enable-static"])
+        if self.settings.compiler == "Visual Studio":
+            self._autotools.defines.append("_WIN32_WINNT=_WIN32_WINNT_WINXP")
+        yes_no = lambda v: "yes" if v else "no"
+        conf_args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+        ]
 
         self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
         return self._autotools
 
     def build(self):
-        autotools = self._configure_autotools()
-        autotools.make()
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.make()
 
     def package(self):
         self.copy(pattern="COPYING*", src=self._source_subfolder, dst="licenses")
-        autotools = self._configure_autotools()
-        autotools.install()
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.install()
 
         os.unlink(os.path.join(self.package_folder, "lib", "libunistring.la"))
         tools.rmdir(os.path.join(self.package_folder, "share"))
